c9fccea6ecf4d715a9264ca5a92f2cc3795b622a
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-06-03 01:28:16 +0200

    basic editor window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+    app.add_plugin(bevy_editor_pls::EditorPlugin::default());
+    app.run();
 }
