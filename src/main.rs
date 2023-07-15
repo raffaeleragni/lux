d5c0092d3e37a98d36d498f6001900566f858893
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-07-15 12:26:10 +0200

    components sub crate

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use bevy::prelude::*;
 
 fn main() {
     let mut app = App::new();
+    components::register(&mut app);
     app.add_plugins(DefaultPlugins);
-    app.add_plugin(bevy_editor_pls::EditorPlugin::default());
+    app.add_plugins(bevy_editor_pls::EditorPlugin::default());
     app.run();
 }
