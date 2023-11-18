a34f4b387972809286d10e72ddbd9b212a4d97e4
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-11-19 00:44:06 +0100

    desktop camera as a module

---
+++ desktop_camera/src/lib.rs
@@ -0,0 +1,7 @@
+use bevy::prelude::*;
+
+pub struct DesktopCameraPlugin;
+
+impl Plugin for DesktopCameraPlugin {
+    fn build(&self, _: &mut App) {}
+}
--- src/desktop.rs
+++ src/desktop.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
+
 mod layouts;
 mod menu;
+
 pub fn init(app: &mut App) {
     app.world.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
@@ -8,5 +10,6 @@ pub fn init(app: &mut App) {
     });
     app.add_plugins(menu::MenuPlugin);
     app.add_plugins(bevy_editor_pls::EditorPlugin::default());
+    app.add_plugins(desktop_camera::DesktopCameraPlugin);
     layouts::init(app);
 }
