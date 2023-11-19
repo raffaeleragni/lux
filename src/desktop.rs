baa012b94b0e0525fc1f4cf67de921da1c1022d5
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-11-19 17:43:07 +0100

    default to noclip at start

--- src/desktop.rs
+++ src/desktop.rs
@@ -1,13 +1,17 @@
 use bevy::prelude::*;
+use desktop_camera::NoClip;
 
 mod layouts;
 mod menu;
 
 pub fn init(app: &mut App) {
-    app.world.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    app.world.spawn((
+        NoClip::default(),
+        Camera3dBundle {
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+    ));
     app.add_plugins(menu::MenuPlugin);
     app.add_plugins(bevy_editor_pls::EditorPlugin::default());
     app.add_plugins(desktop_camera::DesktopCameraPlugin);
