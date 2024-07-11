01d8eb88bb28162f545628df3516d82ccea0f88a
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-07-11 06:24:22 +0200

    fmt

--- crates/lux_xr/src/lib.rs
+++ crates/lux_xr/src/lib.rs
@@ -3,5 +3,5 @@ use bevy_mod_xr::session::XrSessionPlugin;
 
 pub fn init(app: &mut App) {
     info!("Initializing XR");
-    app.add_plugins(XrSessionPlugin{ auto_handle: true });
+    app.add_plugins(XrSessionPlugin { auto_handle: true });
 }
