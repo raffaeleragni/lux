87128747c702acb875e80522f65d6eb318c56cb2
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-07-18 05:58:39 +0200

    fix test

--- crates/lux_components/src/components.rs
+++ crates/lux_components/src/components.rs
@@ -24,30 +24,14 @@ impl<C: Component + Default, F: Component> Component for ControlledBy<C, F> {
     const STORAGE_TYPE: StorageType = StorageType::Table;
     fn register_component_hooks(hooks: &mut ComponentHooks) {
         hooks.on_add(|mut world, entity_id, _component_id| {
-            let entity_mut = &mut world.entity_mut(entity_id);
-            let counter = entity_mut.get_mut::<ControlledByCounter>();
-            if counter.map(|mut c| c.counter += 1).is_none() {
-                world
-                    .commands()
-                    .entity(entity_id)
-                    .insert(ControlledByCounter { counter: 1 });
-            }
+            counter_inc(&mut world, entity_id);
             world
                 .commands()
                 .entity(entity_id)
                 .insert(SyncExclude::<C>::default());
         });
         hooks.on_remove(|mut world, entity_id, _component_id| {
-            let entity_mut = &mut world.entity_mut(entity_id);
-            let counter = entity_mut.get_mut::<ControlledByCounter>();
-            let counter = counter
-                .filter(|c| c.counter > 0)
-                .map(|mut c| {
-                    c.counter -= 1;
-                    c.counter
-                })
-                .unwrap_or(0);
-            if counter == 0 {
+            if counter_dec_and_get(&mut world, entity_id) == 0 {
                 world
                     .commands()
                     .entity(entity_id)
@@ -58,8 +42,42 @@ impl<C: Component + Default, F: Component> Component for ControlledBy<C, F> {
     }
 }
 
+fn counter_inc(world: &mut bevy::ecs::world::DeferredWorld<'_>, entity_id: Entity) {
+    let entity_mut = &mut world.entity_mut(entity_id);
+    match entity_mut.get_mut::<ControlledByCounter>() {
+        Some(mut c) => {
+            c.counter += 1;
+            c.counter
+        }
+        None => {
+            world
+                .commands()
+                .entity(entity_id)
+                .insert(ControlledByCounter { counter: 1 });
+            1
+        }
+    };
+}
+
+fn counter_dec_and_get(
+    world: &mut bevy::ecs::world::DeferredWorld<'_>,
+    entity_id: Entity,
+) -> usize {
+    let entity_mut = &mut world.entity_mut(entity_id);
+    let counter = match entity_mut.get_mut::<ControlledByCounter>() {
+        Some(mut c) => {
+            c.counter -= 1;
+            c.counter
+        }
+        None => 0,
+    };
+    counter
+}
+
 #[cfg(test)]
 mod test {
+    use std::default;
+
     use super::*;
     use bevy::render::primitives::Aabb;
     use bevy_sync::SyncExclude;
@@ -98,15 +116,17 @@ mod test {
     #[test]
     fn test_controlled_by_system_not_removed_with_multiple_controlled_by() {
         let mut app = App::new();
-        let spawn = app
-            .world_mut()
-            .spawn((
-                Name::new(""),
-                ControlledBy::<Name, Aabb>::default(),
-                ControlledBy::<Name, Visibility>::default(),
-            ))
-            .id();
+        let spawn = app.world_mut().spawn(Name::new("")).id();
         app.update();
+        app.world_mut()
+            .commands()
+            .entity(spawn)
+            .insert(ControlledBy::<Name, Name>::default());
+        app.update();
+        app.world_mut()
+            .commands()
+            .entity(spawn)
+            .insert(ControlledBy::<Name, Aabb>::default());
         app.world_mut()
             .commands()
             .entity(spawn)
@@ -114,6 +134,7 @@ mod test {
         app.update();
 
         let spawn = app.world().entity(spawn);
+        assert!(spawn.get::<ControlledByCounter>().is_some());
         assert!(spawn.get::<SyncExclude<Name>>().is_some());
     }
 
